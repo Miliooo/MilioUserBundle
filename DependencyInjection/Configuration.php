d379c6cd814cb84a3c9cb7eadf13de3380d9abf8
michiel <boeckaert@gmail.com>
2014-09-09 03:09:35 +0200

    config updates

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,6 +18,11 @@ class Configuration
     {
         $treeBuilder = new TreeBuilder();
         $rootNode = $treeBuilder->root('milio_user');
+        $rootNode
+            ->children()
+            ->scalarNode('user_read_class')->isRequired()->cannotBeEmpty()->end()
+            ->scalarNode('user_write_class')->isRequired()->cannotBeEmpty()->end()
+        ->end();
 
         return $treeBuilder;
     }
--- DependencyInjection/MilioUserExtension.php
+++ DependencyInjection/MilioUserExtension.php
@@ -24,10 +24,13 @@ class MilioUserExtension extends Extension
         $config = $this->processConfiguration($configuration, $configs);
 
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
-        $loader->load('form.xml');
-        $loader->load('controller.xml');
         $loader->load('command_handlers.xml');
-        $loader->load('provider.xml');
+        $loader->load('controller.xml');
+        $loader->load('form.xml');
         $loader->load('listener.xml');
+        $loader->load('provider.xml');
+
+        $container->setParameter('milio_user.user_read_class', $config['user_read_class']);
+        $container->setParameter('milio_user.user_write_class', $config['user_write_class']);
     }
 }
